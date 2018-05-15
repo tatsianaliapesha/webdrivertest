b158952bdf14b390ad6471ac43f37478d99de8e3
Tatsiana Liapesha <tatsiana_liapesha@epam.com>
2018-05-15 13:31:15 +0300

    Update server URL.

--- src/test/java/com/globoforce/testautomation/mentoring/webdriver/test/scenarios/BaseConversationsTest.java
+++ src/test/java/com/globoforce/testautomation/mentoring/webdriver/test/scenarios/BaseConversationsTest.java
@@ -7,12 +7,13 @@ import org.testng.annotations.Parameters;
 
 public class BaseConversationsTest extends BaseTest {
 
-    private static final String URL = "https://test-web3-05.corp.globoforce.com/conversations/#!dashboard/%s/%s";
+    //private static final String URL = "https://test-web3-05.corp.globoforce.com/conversations/#!dashboard/%s/%s";
+    private static final String URL = "https://test-web3-05.corp.globoforce.com/conversations/#!dashboard/%s";
 
     @BeforeClass
     @Parameters({"clientName", "userId", "userName", "password"})
     public void loginToConversations(String clientName, String userId, String userName, String password) {
-        driver.get(String.format(URL, clientName, userId));
+        driver.get(String.format(URL, clientName));
         driver.findElement(By.name("username")).sendKeys(userName);
         driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.id("signIn-button")).click();
